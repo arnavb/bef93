bdcac33c3d75365b7131ef14832186de26a59121
Arnav Borborah <arnavborborah11@gmail.com>
2018-11-28 15:02:15 +0000

    Add Apache 2.0 License

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Bef93
 
-Bef93 is a simple befunge interpreter written in Rust.
+Bef93 is a simple Befunge-93 interpreter written in Rust.
+
+## License
+
+This project is licensed under the [Apache 2.0 License](./LICENSE).
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+/* main.rs - Contains the main function and CLI code for bef93.
+ * Copyright 2018 Arnav Borborah
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #[macro_use] extern crate clap;
 
-mod interpreter;
+mod bef93;
 
 use std::{error, io, process};
 use std::env::current_dir;
