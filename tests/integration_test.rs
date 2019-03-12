8506fd6ebc4241983e8771961f38a1652b7e7dce
Arnav Borborah <arnavborborah11@gmail.com>
2019-03-12 02:35:54 +0000

    Finish writing integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,8 @@
 extern crate assert_cmd;
 
 use assert_cmd::prelude::*;
+
+use std::env::current_dir;
 use std::process::Command;
 
 const EXPECTED_HELP_MESSAGE: &str = "bef93 0.1.0
@@ -62,6 +64,30 @@ fn test_short_version_message() {
 fn test_invalid_arguments() {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
     cmd.arg("random").arg("--arguments");
+
+    cmd.assert().failure().code(1);
+}
+
+#[test]
+fn test_example_file() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.arg(current_dir().unwrap().join("tests").join("hello_world.bf"));
+
+    cmd.assert().success().code(0).stdout("Hello, World!\n");
+}
+
+#[test]
+fn test_file_not_found() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.arg("non_existent.bf");
+
+    cmd.assert().failure().code(1);
+}
+
+#[test]
+fn test_invalid_file_extension() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.arg(current_dir().unwrap().join("tests").join("integration_test.rs"));
 
     cmd.assert().failure().code(1);
 }
